8170fc517409f7c1d3e724f8da27ad8aa9a756af
tabrez.shaikh <tabrez.shaikh@netcore.co.in>
2016-02-11 19:52:35 +0530

    modified params and algo as per Pepipost requirements

--- pepipost.php
+++ pepipost.php
@@ -1,63 +1,92 @@
 <?php
 /*
- * File: webhooks/mandrill.php
- * Description: This file provides an endpoint for Mandrill's API.
+ * File: webhooks/pepipost.php
+ * Description: This file provides an endpoint for Pepipost's API.
  * Version: 0.1
  * Contributors:
- *      Mandrill API    https://mandrillapp.com/api/docs/webhooks.php.html
+ *      Pepipost API    https://docs.pepipost.com/
+ *      Tabrez Shaikh    http://tabrez.me/
  *      Blaine Moore    http://blainemoore.com
- *
- * Requirements:
- *      $webhooks_critsend_api_key must be set in config.php (Sendy's or Webhook's)
  */
 
+/**
+* Example output:
+{
+"TRANSID":"14551290015805717",
+"RCPTID":"12",
+"RESPONSE":"smtp;250 2.0.0 Ok: queued as 824C61FBBB3\r",
+"EMAIL":"toabc@abc.com",
+"TIMESTAMP":"1455193720",
+"CLIENTID":"18654",
+"FROMADDRESS":"fromabc@abc.com",
+"EVENT":"sent",
+"USERAGENT":"1468"
+}
+
+// for debugging
+function webhooks_debug($msg){
+    $fp =fopen('webhooklog.txt','a');
+    fwrite($fp,"\nmsg($msg)<br />\n");
+    fclose($fp);
+}
+*/
 include_once('includes/config.php');
-$webhooks_provider = "Mandrill";
+$webhooks_provider = "Pepipost";
 
 $HTTP_RAW_POST_DATA = @file_get_contents('php://input');
-$dec_url = urldecode($HTTP_RAW_POST_DATA);
-$json_payload = substr($dec_url, 16);
-$events = json_decode($json_payload, true);
+$json_payload = urldecode($HTTP_RAW_POST_DATA);
 
-foreach($events as $event)
-{
-    if (filter_var($event['msg']['email'],FILTER_VALIDATE_EMAIL)) {
-        switch($event["event"])
-        {
-            case "send": event_send($event); break;
-            case "deferral": webhooks_soft_bounce($event['msg']['email'], "deferral: " . $event['msg']['bounce_description']); break;
-            case "hard_bounce": webhooks_hard_bounce($event['msg']['email'], $event['msg']['bounce_description']); break;
-            case "soft_bounce": webhooks_soft_bounce($event['msg']['email'], $event['msg']['bounce_description']); break;
-            case "open": event_open($event); break;
-            case "click": event_click($event); break;
-            case "spam": webhooks_spam_report($event['msg']['email']); break;
-            case "spamreport": webhooks_spam_report($event['msg']['email']); break;
-            case "unsub": event_unsubscribe($event); break;
-            case "reject": webhooks_hard_bounce($event['msg']['email'], "reject: " . $event['msg']['bounce_description']); break;
-            default: webhooks_debug(" == Invalid category: '".$event["category"]."' for: ".$event["recipient"]." ==");
-        }
-    } else { // invalid email address
-        webhooks_debug(" == Invalid email address: '".$event['msg']['email']."' ==");
-    } // if (filter_var($event["recipient"],FILTER_VALIDATE_EMAIL))
-} // foreach($events as $event)
+$event = json_decode($json_payload, true);
+
+if (filter_var($event['EMAIL'],FILTER_VALIDATE_EMAIL)) {
+    switch($event["EVENT"])
+    {
+        case "sent": event_send($event); break;
+        case "opened": event_open($event); break;
+        case "clicked": event_click($event); break;
+        case "bounced":
+            if($event['BOUNCE_TYPE'] === 'HARDBOUNCE') {
+                webhooks_hard_bounce($event['EMAIL']);
+            }
+            else {
+                webhooks_soft_bounce($event['EMAIL']);
+            }
+            break;
+        case "unsubscribed": event_unsubscribe($event); break;
+        case "abuse": webhooks_spam_report($event['EMAIL']);
+        case "dropped": event_dropped($event); break;
+        case "invalid": event_invalid($event); break;
+        default: webhooks_debug(" == Invalid category: '".$event["EVENT"]."' for: ".$event["EMAIL"]." ==");
+    }
+} else { // invalid email address
+    webhooks_debug(" == Invalid email address: '".$event['EMAIL']."' ==");
+} // if (filter_var($event["recipient"],FILTER_VALIDATE_EMAIL))
 
 //----------------------------------------------------------------------------------//
 //              MANDRILL EVENTS UNHANDLED BY WEBHOOKS
 //----------------------------------------------------------------------------------//
     function event_open($event) {
-        webhooks_debug("Email opened: " . $event['msg']['email'] . " (" . $event['msg']['subject'] . ")\t(Currently unhandled by Webhooks)");
+        webhooks_debug("Email opened: " . $event['EMAIL'] . " (" . $event['TRANSID'] . ")\t(Currently unhandled by Webhooks)");
     } // function event_open($event)
 
     function event_click($event) {
-        webhooks_debug("Email clicked: " . $event['msg']['email'] . " (" . $event['msg']['clicks']['url'] . ")\t(Currently unhandled by Webhooks)");
+        webhooks_debug("Email clicked: " . $event['EMAIL'] . " (" . $event['TRANSID'] . ")\t(Currently unhandled by Webhooks)");
     } // function event_click($event)
 
     function event_unsubscribe($event) {
-        webhooks_debug("Email unsubscribed: " . $event['msg']['email'] . "\t(Currently unhandled by Webhooks)");
+        webhooks_debug("Email unsubscribed: " . $event['EMAIL'] . " (" . $event['TRANSID'] . ")\t(Currently unhandled by Webhooks)");
     } // function event_unsubscribe($event)
 
     function event_send($event) {
-        webhooks_debug("Email sent: " . $event['msg']['email'] . "\t(Currently unhandled by Webhooks)");
+        webhooks_debug("Email sent: " . $event['EMAIL'] . " (" . $event['TRANSID'] . ")\t(Currently unhandled by Webhooks)");
     } // function event_send($event)
 
+    function event_dropped($event) {
+        webhooks_debug("Email dropped: " . $event['EMAIL'] . " (" . $event['TRANSID'] . ")\t(Currently unhandled by Webhooks)");
+    } // function event_dropped($event)
+
+    function event_invalid($event) {
+        webhooks_debug("Email invalid: " . $event['EMAIL'] . " (" . $event['TRANSID'] . ")\t(Currently unhandled by Webhooks)");
+    } // function event_invalid($event)
 //----------------------------------------------------------------------------------//
+
